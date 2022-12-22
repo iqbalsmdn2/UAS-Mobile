3984a7a88dfa1462a63d6854ecc47c511298014c
Muhamad Iqbal Wijaksana <iqbalsdmn2@gmail.com>
2022-12-22 21:15:42 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/course_reminder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.course_reminder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'screen/home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+  var myBox = await Hive.openBox('course_box');
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UAS PMP',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
